6acdce426b0ee972b4b2d468ebd22c64bba81d66
jackyh1999 <jacky.hong88@gmail.com>
2021-10-17 19:32:38 +0800

    test new function
    descirption, order, order_list

--- line_bot.py
+++ line_bot.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('KbUSP5ShwG5gziWRdy3niYUieAZaYlDc2YMW1HB3Ao05YRm+DKUar29lK0lfqjeMqzLRm1MLALf/R4jIV/k+98YxIR40SryCI8qsokVBe31heMMafyPQSI89odk42Ts1dD9b35gyPMCkOhHEGp+M/wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('b33a01e1e548c7b39a732d62245e1d36')
 
-# 此為 Webhook callback endpoint
+# Webhook callback endpoint
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -28,7 +28,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body（負責）
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -37,15 +37,31 @@ def callback():
 
     return 'OK'
 
+text = ''
+description = '指令輸入格式: (指令)/ (內容)\n指令: 說明、點餐、點餐清單'
+order_list = ''
 
-# decorator 負責判斷 event 為 MessageEvent 實例，event.message 為 TextMessage 實例。所以此為處理 TextMessage 的 handler
+# decorator 判斷 event 為 MessageEvent
+# event.message 為 TextMessage 
+# 所以此為處理 TextMessage 的 handler
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 決定要回傳什麼 Component 到 Channel
+    '''
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
+    '''
+    message = event.message.text
+    texts = message.split(1)
+    if(texts[0] == '說明/')
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(description))
+    if(texts[0] == '點餐/')
+        profile = line_bot_api.get_profile(user_id)
+        order_list.append(profile.display_name + ' ' + texts[1] + '\n')
+    if(texts[0] == '點餐清單/')
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(order_list))
+            
 
 if __name__ == '__main__':
     app.run()
