aea96a31d9f07ab4320e66b64eb40f3ad784d095
gangan <44292094+jackyh1999@users.noreply.github.com>
2021-10-22 10:23:46 +0800

    add restaurant and find bug

--- line_bot.py
+++ line_bot.py
@@ -148,6 +148,7 @@ def handle_message(event):
         jsonFile.close()
         orderFile.close()        
         
+        '''
         # create and send order datail image
         img = Image.new('RGB', (600, 800), color=(255, 255, 255))
         font = ImageFont.truetype('data/arial.ttf', size = 24)
@@ -156,7 +157,7 @@ def handle_message(event):
         
         img.save('data/detail.png')
         line_bot_api.reply_message(event.reply_token, ImageSendMessage('data/detail.png', 'data/detail.png'))
-        
+        '''
 
 
     elif texts[0] == 'clear':
